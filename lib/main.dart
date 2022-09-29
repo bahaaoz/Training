c1ee5cadc79389f76c3b1fa30ef5cda718fd6af8
Bahaa <CS Net Games@Bahaa.bahaa>
2022-09-29 11:12:12 +0300

    move all files try4

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:project_thered/Splash.dart';
+import 'package:project_thered/first.dart';
+import 'package:project_thered/second.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: "Roboto"),
+      debugShowCheckedModeBanner: false,
+      initialRoute: "splash",
+      routes: {
+        "first": (context) => First(),
+        "second": (context) => second(),
+        "splash": (context) => Splash(),
+      },
+      home: const Scaffold(),
+    );
+  }
+}
---
+++ lib/second.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:project_thered/first.dart';
+
+class second extends StatefulWidget {
+  second({Key? key}) : super(key: key);
+
+  @override
+  State<second> createState() => _secondState();
+}
+
+class _secondState extends State<second> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.home),
+          onPressed: () {
+            finalResult = "";
+            Navigator.of(context).pop();
+            Navigator.of(context).pop();
+            Navigator.of(context).pushNamed("first");
+          },
+        ),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text(
+          finalResult,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            fontFamily: "Toledo",
+          ),
+        ),
+      ),
+    );
+  }
+}
